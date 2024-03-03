fc7ee2ca4eb29062c7e9c3cfe760a2495dd416e5
frdor <galushin@sel-be.ru>
2024-03-03 18:51:02 +0300

    result

--- src/test/java/testHome/DemoQA.java
+++ src/test/java/testHome/DemoQA.java
@@ -1,9 +1,11 @@
 package testHome;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.io.File;
 
@@ -25,14 +27,15 @@ public class DemoQA {
         String lastName = "house";
         String email = "teatF@mail.ru";
         String number = "9118883333";
-        String date = "04.04.2024";
+        String date = "22 April,2015";
         open("automation-practice-form");
         $("#firstName").setValue(name);
         $("#firstName").setValue(name);
         $("#lastName").setValue(lastName);
         $("[for='gender-radio-1']").click();
         $("#userNumber").setValue(number);
-        $("#dateOfBirthInput").setValue(date).pressEnter();
+        actions().click($("#dateOfBirthInput")).click().click().sendKeys(date).perform();
+        $("#dateOfBirthInput").pressEnter();
         $("#lastName").click();
         $("[for='hobbies-checkbox-1']").click();
         File file = new File("src/test/file/Выделение_143.png");
@@ -42,12 +45,14 @@ public class DemoQA {
         $("#submit").click();
 
         String resultFIO = $("tbody > tr:nth-child(1) > td:last-child").getText();
-        String resultDate = $("tbody").$(byText("Date of Birth")).sibling(0).getText();
-        System.out.println(resultDate + "++++++++++++++");
+        SelenideElement element = $("tbody").$(byText("Date of Birth")).sibling(0);
+        String resultDate = element.getText();
+        String resultFileName = $("tbody").$(byText("Picture")).sibling(0).getText();
 
         assertAll(
                 () -> assertEquals(name + " " + lastName, resultFIO),
-                () -> assertEquals(name + " " + lastName, resultFIO)
+                () -> assertEquals(date, resultDate),
+                () -> assertEquals("Выделение_143.png",resultFileName)
         );
         sleep(2000);
     }
